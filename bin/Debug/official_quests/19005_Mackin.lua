32d33d80c23e7f8d2f18975405b482c5e1884614
AKUMA <akuma@xskip.net>
2013-10-14 09:58:22 +0300

    LUA : 19005_Mackin [ @xDarkParadisE ]

--- bin/Debug/official_quests/19005_Mackin.lua
+++ bin/Debug/official_quests/19005_Mackin.lua
@@ -1,15 +1,20 @@
 local NPC = 19005;
 
 if EVENT == 100 then
+   SelectMsg(UID, 3, -1, 8075, NPC, 4481, 101, 3019, 102);
+end
+
+
+if EVENT == 101 then
    MONEY = HowmuchItem(UID, 900000000);
    if MONEY >= 1000000 then
-      SelectMsg(UID, 3, -1, 8075, NPC, 4481, 205, 3019, 203);
+      SelectMsg(UID, 3, -1, 8075, NPC, 4484, 103, 4296, 104);
    else
       SelectMsg(UID, 2, -1, 11338, NPC, 27);
    end
 end
 
-if EVENT == 205 then
+if EVENT == 103 then
    GoldLose(UID, 1000000)
    SelectMsg(UID, 16, -1, NPC);
 end
