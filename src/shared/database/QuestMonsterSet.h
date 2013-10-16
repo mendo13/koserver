7926707f0efa087d0a87173f3c0847e94870c662
katanatr <katanatr@outlook.com>
2013-10-16 19:08:34 +0300

    QUEST_MONSTER tablosu için eksik sutun eklendi

--- src/shared/database/QuestMonsterSet.h
+++ src/shared/database/QuestMonsterSet.h
@@ -7,7 +7,7 @@ public:
 		: OdbcRecordset(dbConnection), m_pMap(pMap) {}
 
 	virtual tstring GetTableName() { return _T("QUEST_MONSTER"); }
-	virtual tstring GetColumns() { return _T("sNum1a, sNum1b, sNum1c, sNum1d, sCount1, sNum2a, sNum2b, sNum2c, sNum2d, sCount2, sNum3a, sNum3b, sNum3c, sNum3d, sCount3, sNum4a, sNum4b, sNum4c, sNum4d, sCount4"); }
+	virtual tstring GetColumns() { return _T("sQuestNum, sNum1a, sNum1b, sNum1c, sNum1d, sCount1, sNum2a, sNum2b, sNum2c, sNum2d, sCount2, sNum3a, sNum3b, sNum3c, sNum3d, sCount3, sNum4a, sNum4b, sNum4c, sNum4d, sCount4"); }
 
 	virtual bool Fetch()
 	{
