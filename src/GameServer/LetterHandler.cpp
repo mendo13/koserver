723a338ff1f853870db882d0ccf5868fb17d1189
mendo13 <cristianforce3@hotmail.com>
2013-12-04 20:10:08 +0100

    Patch2: Fix Bound Items(Old jewlry &&Krowaz Armor)
    can't send by letter bound items

--- src/GameServer/LetterHandler.cpp
+++ src/GameServer/LetterHandler.cpp
@@ -162,7 +162,7 @@ void CUser::ReqLetterSend(Packet & pkt)
 				bResult = -1;
 		// Untradeable item
 		else if (pTable->m_bRace == RACE_UNTRADEABLE || nItemID >= ITEM_GOLD
-			|| pItem->isSealed() || pItem->isRented())
+			|| pItem->isSealed() || pItem->isRented() || pItem->isBound())
 			bResult = -32;
 	}
 
